d41baa2f259489c92cc86baaa589216ee7d6aedc
Bartossh <lenart.consulting@gmail.com>
2024-07-28 18:00:01 +0200

    Combine AhoCorasick with regex and tokenizer.

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,9 +1,5 @@
-use std::collections::HashMap;
-
 use criterion::{criterion_group, criterion_main, Criterion};
-use static_detector::detection::inspection::Inspector;
-use static_detector::preprocessor::cleanup_large_spaces;
-use tokenizers::pre_tokenizers::whitespace::Whitespace;
+use static_detector::generic_detector::{Builder, Scanner};
 use tokenizers::{OffsetReferential, OffsetType, PreTokenizedString, PreTokenizer};
 
 const TEST_CRIME_AWS_GCP: &str = r#"
@@ -45,211 +41,48 @@ aws_session_token=fcZib3JpZ2luX2IQoJb3JpZ2luX2IQoJb3JpZ2luX2IQoJb3JpZ2luX2IQoJb3
 ]
 "#;
 
-const TEST_CRIME_TWITTER: &str = r#"
-[
-  {
-    "Api Key":                "5PAgMqo1gXn7QyHzzGASDFN9Q",
-    "Api Key Secret":         "PDrhKbxeYhxeWz6R1UddxdJFXCRsZadTtsDmwlUBxPGB4bU2aU",
-    "Bearer Token":           "AAAAAAAAAAAAAAAAAAAAAOokiwEAAAAAINuBw0mowW3KpPh0zxGB2vgGY7g%3DXePv83wPJ5VOvROMkfGwkPtptd5w9xAAGUHrgfkhppjXA2JWN5",
-    "Access Token":           "955116647488028672-NvXaA5BEnf9gYAcK40hNZGTbPdlwmaU",
-    "Access Token Secret":    "H1xeoEa7i6PnarMvpKIz2WiVgqJetEmnMRlRBVaZrOekd"
-  },
-  {
-    "Api Key":                "IwSJrHZeuP1Hl9Edz9VQyqT7x",
-    "Api Key Secret":         "hR7vHE72b5IIoKGh0icOBprExoZkamRrvjidXDadn5gudqff6z",
-    "Bearer Token":           "AAAAAAAAAAAAAAAAAAAAAOokiwEAAAAAwi3BY9v2NbI1%2BsINLKs0gNCF%2Fio%3DRwh43tAmNVdRVTzkvUx29gQ0exqGtFaQyTffzI1juiicxuT2W7",
-    "Access Token":           "955116647488028672-dde5wWYS29lJUVaKoktDvyMlJS0zsKY",
-    "Access Token Secret":    "BRyuRAFLPwQH1W0vIQlVxcJvbfOUV5j5xdUj4KbALU8RC"
-  },
-  {
-    "Api Key":                "PyjocY7kHdiekmGsi6ndao2hR",
-    "Api Key Secret":         "16hHCNCpykZliz6juLF5xkHuGniueh4PFRRjKLLnfhuuNjJPyE",
-    "Bearer Token":           "AAAAAAAAAAAAAAAAAAAAAOokiwEAAAAASO6z6R5Iw8rIb8cCspGTp%2B6ERwo%3Dvt0hpY8WXq0HJisC9aikQn8ocDZLEIXSdoLTezIBcFlzLUkzJY",
-    "Access Token":           "955116647488028672-RdYFDr6FkMrZN4bk3O4stZj3xqDc7AM",
-    "Access Token Secret":    "69yZI4oq1IZXEtr2T6BQo05KqQLadzphmEqrI5rWRUHW7"
-  },
-  {
-    "Api Key":                "oYBUjCeergTCyrbvhGhMQ6vG4",
-    "Api Key Secret":         "sp1mizKQPorh0UCCL0G6WZVvtD15ourLcTOKutSiYywCl851QX",
-    "Bearer Token":           "AAAAAAAAAAAAAAAAAAAAAOokiwEAAAAAZv3%2BaU5t0Vtzwxa69CpXFLcG72E%3DqYWvGr4WKYaebn6qypSsRw3xzMvK1g4KciyYVqrPYwMVvPAgzL",
-    "Access Token":           "955116647488028672-VPc28faQNZtHp0fikUhNFAcophLbcBH",
-    "Access Token Secret":    "1zJUU8Nfeg0JyFNGGl3HqiW4eHvCD9PWRSINmdUBx4SXg"
-  },
-  {
-    "Api Key":                "04NoreYdRRE7JyYSTagqUIhkm",
-    "Api Key Secret":         "WfRLkulliRNtWaTEGmoTllgyB01di83cnRjPNMBGpkpGgd6SM0",
-    "Bearer Token":           "AAAAAAAAAAAAAAAAAAAAAOokiwEAAAAAEBVfpu94kepJoMULqERWxIL7g68%3DC3Jy2zaUnTrHNxZNRSrlOCIOjoBVyPDILDaffVoxxcQzwkPoGj",
-    "Access Token":           "955116647488028672-iIPUHS2KA6t2A1MMuA71e9ujmrvLfo6",
-    "Access Token Secret":    "QDmKy2R8Sy3owzAU5fn6Fm6nXQqvChybdp5Ha4zIy5jT9"
-  },
-  {
-    "Api Key":                "53kDReVLIm9ekk7a7kWHxdQ6c",
-    "Api Key Secret":         "f7a1xMAUjJMsd76SyfuqxIZiZvFDXTkaLE0OHaPZDpcG7YJ2Rb",
-    "Bearer Token":           "AAAAAAAAAAAAAAAAAAAAAOokiwEAAAAAM%2BcWWEICaBtt9kG6ho8WK9I%2FoTk%3DX3U2VpxkKYUEikurhLgV9HmuwMS61Pz94dp9IQkVJ1RAFNNFKi",
-    "Access Token":           "955116647488028672-pTjShSi8sMSrBjMIvZH2cs8glCayepI",
-    "Access Token Secret":    "hJGLZDjVIRBLu5v3YxMg7GzQ36GbJ1eW2zhCkwsHYSD0H"
-  },
-  {
-    "Api Key":                "LPTtKbnTGgnzj4XrCBCYSqRcP",
-    "Api Key Secret":         "nahpwSPzIsg14Z43O9o9anKbuCWYL6ZyjymrJDxngMxRLFxKfR",
-    "Bearer Token":           "AAAAAAAAAAAAAAAAAAAAAOokiwEAAAAAU7sfX1jG7feA1J3x%2FLOG4eioLy0%3DqW2ryZPU3PPo8yajka1CKgEDSFMjPRQijhydINoNBRqtZZKVUq",
-    "Access Token":           "955116647488028672-QoKBPDd1td4Ngyp669xvZBFLjvEqrSh",
-    "Access Token Secret":    "1PO1ztph22zLU39QZxqpcKlSoSugpsZSNClSROIu9JELs"
-  },
-  {
-    "Api Key":                "NCYWHSACCsWdyVAYXZxb5cj30",
-    "Api Key Secret":         "bsipEFMsDAJLS4Ac4xB1WWOGwtT2OfLgmPO4EtNXgnJGakgOQS",
-    "Bearer Token":           "AAAAAAAAAAAAAAAAAAAAAOokiwEAAAAAkVMyeUwCzFuIkozZ%2BWvu4li9wZg%3D2Q3dzqJZg41Hz7IYahNuY8VbA1lAzvi0ftOYt9UcC3BTAN1ssM",
-    "Access Token":           "955116647488028672-v09GALnNJIyprRoly7y35ppWOT8cTuJ",
-    "Access Token Secret":    "bAXEnYpxgIA9DetJPGbPRvIvOy6AugxnBQSnaYl1dvGqi"
-  },
-  {
-    "Api Key":                "Wzd3WBYr9HhThzhFMBlYMLE9S",
-    "Api Key Secret":         "K7Hw32JdwfCC3Iwgm6rgh3EUjiweyLD8L3tPC98VnDsQ715S5G",
-    "Bearer Token":           "AAAAAAAAAAAAAAAAAAAAAOokiwEAAAAAZVe4Znhi14HRCDDQHWZj4JhDT1I%3DUlwU7UWD90SmfWetqc2mLCl6NzaIWvTQeHusTGdvDdB0eAbtmj",
-    "Access Token":           "955116647488028672-K4uKNEEep8jMHHcPLzFOWORzuK6yHX4",
-    "Access Token Secret":    "5NS4g8AL56zI4rnnufTMujG0lXUk5KUAeKTjB22RFnFpl"
-  },
-  {
-    "Api Key":                "QPLHSazDh18efjex1DCe9vqSV",
-    "Api Key Secret":         "kskbOBkj7dk3h7Kee6VmVrKp8Lo4m5socnFE9Z3pOSZH7oXi0C",
-    "Bearer Token":           "AAAAAAAAAAAAAAAAAAAAAOokiwEAAAAAQKu0SOsxZH4b9cz0NVWkIRxfxK4%3D0aR3NAlIjohUPj1lf39LirIeSO2y4RvsDGPFRgUiFuUD52YFqR",
-    "Access Token":           "955116647488028672-JKMlfU95YvQqjmAjoNsJbQjQdBUtVlA",
-    "Access Token Secret":    "vW36iduHNPQAq0EZobWTDDDCmXVPC3dmzYNjc7yORkQQR"
-  }
-]"#;
-
-const TEST_JSON: &str = r#"{
-  "Api Key":                "5PAgMqo1gXn7QyHzzGASDFN9Q",
-  "Api Key Secret":         "PDrhKbxeYhxeWz6R1UddxdJFXCRsZadTtsDmwlUBxPGB4bU2aU",
-  "Bearer Token":           "AAAAAAAAAAAAAAAAAAAAAOokiwEAAAAAINuBw0mowW3KpPh0zxGB2vgGY7g%3DXePv83wPJ5VOvROMkfGwkPtptd5w9xAAGUHrgfkhppjXA2JWN5",
-  "Access Token":           "955116647488028672-NvXaA5BEnf9gYAcK40hNZGTbPdlwmaU",
-  "Access Token Secret":    "H1xeoEa7i6PnarMvpKIz2WiVgqJetEmnMRlRBVaZrOekd"
-}"#;
-
-fn benchmark_inspector_for_assets_config_aws_gcp_yaml(c: &mut Criterion) {
-    c.bench_function("benchmark_inspector_for_assets_config_aws_gcp_yaml", |b| {
-        let Ok(inspector) = Inspector::try_new("./assets/config.yaml") else {
-            assert!(false);
-            return;
-        };
-
-        b.iter(|| {
-            let _ = inspector.scan(TEST_CRIME_AWS_GCP);
-        });
-    });
-}
-
-fn benchmark_inspector_for_assets_config_aws_yaml(c: &mut Criterion) {
-    c.bench_function("benchmark_inspector_for_assets_config_aws_yaml", |b| {
-        let Ok(inspector) = Inspector::try_new("./assets/config_aws.yaml") else {
-            assert!(false);
-            return;
-        };
-
-        b.iter(|| {
-            let _ = inspector.scan(TEST_CRIME_AWS_GCP);
-        });
-    });
-}
-
-fn benchmark_inspector_for_assets_config_gcp_yaml(c: &mut Criterion) {
-    c.bench_function("benchmark_inspector_for_assets_config_gcp_yaml", |b| {
-        let Ok(inspector) = Inspector::try_new("./assets/config_gcp.yaml") else {
-            assert!(false);
-            return;
-        };
-
-        b.iter(|| {
-            let _ = inspector.scan(TEST_CRIME_AWS_GCP);
-        });
-    });
-}
-
-fn benchmark_inspector_for_assets_config_twitter_yaml(c: &mut Criterion) {
-    c.bench_function("benchmark_inspector_for_assets_config_twitter_yaml", |b| {
-        let Ok(inspector) = Inspector::try_new("./assets/config_twitter.yaml") else {
-            assert!(false);
-            return;
-        };
-
-        b.iter(|| {
-            let _ = inspector.scan(TEST_CRIME_TWITTER);
-        });
-    });
-}
-
-fn benchmark_inspector_for_assets_config_twitter_yaml_json_parse(c: &mut Criterion) {
+fn benchmark_generic_detector_create_scanner(c: &mut Criterion) {
     c.bench_function(
-        "benchmark_inspector_for_assets_config_twitter_yaml_json_parse",
+        "benchmark_generic_detector_create_scanner",
         |b| {
-            let Ok(inspector) = Inspector::try_new("./assets/config_twitter.yaml") else {
-                assert!(false);
-                return;
-            };
-
             b.iter(|| {
-                let _ = inspector.scan(TEST_JSON);
+                let Ok(scanner) = Builder::new().
+                    with_splits(&[", ", " - ", " + ", " / ", " * "]).
+                    with_secret_regexes(&[r#"KEY-----[\a-zA-Z0-9]*-----END"#]).
+                    with_variables(&["auth_uri", "token_uri","auth_provider_x509_cert_url"], &[r#"https://[a-zA-Z-0-9./]*"#]).try_build_scanner() else {
+                            assert!(false);
+                            return;
+                        };
             });
-        },
+        }
     );
 }
 
-fn benchmark_inspector_for_cleanup_large_spaces(c: &mut Criterion) {
-    c.bench_function("benchmark_inspector_for_cleanup_large_spaces", |b| {
-        b.iter(|| {
-            let _ = cleanup_large_spaces(TEST_CRIME_TWITTER);
-        });
-    });
-}
-
-fn benchmark_pre_tokenizer(c: &mut Criterion) {
-    c.bench_function("benchmark_pre_tokenizer", |b| {
-        b.iter(|| {
-            let pre_tokenizer = Whitespace {};
-            let mut pre_tokenized = PreTokenizedString::from(TEST_CRIME_AWS_GCP);
-            let Ok(_) = pre_tokenizer.pre_tokenize(&mut pre_tokenized) else {
-                assert!(false);
-                return;
-            };
-        });
-    });
-}
-
-fn benchmark_pre_tokenizer_loop_spits_push_hash_map_1000_inner_loops(c: &mut Criterion) {
+fn benchmark_generic_detector_scan_with_scanner(c: &mut Criterion) {
     c.bench_function(
-        "benchmark_pre_tokenizer_loop_spits_push_hash_map_1000_inner_loops",
+        "benchmark_generic_detector_scan_with_scanner",
         |b| {
-            let pre_tokenizer = Whitespace {};
-            let mut pre_tokenized = PreTokenizedString::from(TEST_CRIME_AWS_GCP);
-            let Ok(_) = pre_tokenizer.pre_tokenize(&mut pre_tokenized) else {
-                assert!(false);
-                return;
-            };
+            let Ok(scanner) = Builder::new().
+                with_splits(&[", ", " - ", " + ", " / ", " * "]).
+                with_secret_regexes(&[r#"KEY-----[\a-zA-Z0-9]*-----END"#]).
+                with_variables(&["auth_uri", "token_uri","auth_provider_x509_cert_url"], &[r#"https://[a-zA-Z-0-9./]*"#]).try_build_scanner() else {
+                        assert!(false);
+                        return;
+                    };
             b.iter(|| {
-                let mut h = HashMap::new();
-                let mut counter = 1;
-                for split in pre_tokenized.get_splits(OffsetReferential::Original, OffsetType::Byte)
-                {
-                    for next in 0..1000 {
-                        h.insert(next * counter, split);
-                    }
-                    counter += 1;
-                }
+                let Ok(()) = scanner.scan(TEST_CRIME_AWS_GCP) else {
+                    assert!(false);
+                    return;
+                };
             });
-        },
+        }
     );
 }
+
 
 criterion_group!(
     benches,
-    benchmark_inspector_for_assets_config_aws_gcp_yaml,
-    benchmark_inspector_for_assets_config_aws_yaml,
-    benchmark_inspector_for_assets_config_gcp_yaml,
-    benchmark_inspector_for_assets_config_twitter_yaml,
-    benchmark_inspector_for_assets_config_twitter_yaml_json_parse,
-    benchmark_inspector_for_cleanup_large_spaces,
-    benchmark_pre_tokenizer,
-    benchmark_pre_tokenizer_loop_spits_push_hash_map_1000_inner_loops
+    benchmark_generic_detector_create_scanner,
+    benchmark_generic_detector_scan_with_scanner,
 );
 criterion_main!(benches);
--- src/generic_detector/mod.rs
+++ src/generic_detector/mod.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
+use aho_corasick::AhoCorasick;
 use tokenizers::pre_tokenizers::delimiter::CharDelimiterSplit;
 use tokenizers::pre_tokenizers::{sequence::Sequence, split::Split};
 use tokenizers::tokenizer::normalizer::SplitDelimiterBehavior;
 use tokenizers::{OffsetReferential, OffsetType, PreTokenizedString, PreTokenizer};
-
+use regex::{RegexBuilder, Regex};
 
 /// Scanner offers scanning capabilities.
 /// Scanning returns result of all found secrets locations.
@@ -13,10 +14,15 @@ pub trait Scanner {
 }
 
 #[derive(Debug)]
+struct VariableSchema {
+    aho: AhoCorasick,
+    reg: Vec<Regex>,
+}
+
+#[derive(Debug)]
 struct Scan{
-    secret_regex: Vec<String>,
-    variables: Vec<String>,
-    assigments: Vec<String>,
+    secret_regex: Vec<Regex>,
+    variables_schema: Option<VariableSchema>,
     pre_tokenizer: Sequence,
 }
 
@@ -28,17 +34,27 @@ impl Scanner for Scan {
 }
 
 #[derive(Debug)]
+struct Location {
+    variable: usize,
+    value: usize,
+}
+
+#[derive(Debug)]
+struct Candidate {
+    variable: usize,
+    secret: usize,
+}
+
+#[derive(Debug)]
 struct Detection<'a> {
-    tokens_locations: HashMap<String, u32>,
-    assignments_locations: HashMap<u32, u32>,
+    variable_assignemnts: HashMap<String, Location>,
     scanner: &'a Scan,
 }
 
 impl<'a> Detection<'a> {
     fn new(s: &'a Scan) -> Self {
         Self {
-            tokens_locations: HashMap::new(),
-            assignments_locations: HashMap::new(),
+            variable_assignemnts: HashMap::new(),
             scanner: s,
         }
     }
@@ -48,44 +64,68 @@ impl<'a> Detection<'a> {
         if let Err(err)  = self.scanner.pre_tokenizer.pre_tokenize(&mut pre_tokenized) {
             return Err(err.to_string());
         }
-        for token in pre_tokenized
-            .get_splits(OffsetReferential::Original, OffsetType::Byte)
-            .iter()
+
+        let tokens = pre_tokenized.get_splits(OffsetReferential::Original, OffsetType::Byte);
+        let mut candidates = Vec::new(); // TODO: run per variable_schema
+        let mut variable_idx: Option<usize> = None; // TODO: run per variable_schema shall be more then one in the future
+        for (i, token) in tokens.iter().enumerate()
         {
-            println!("TOKEN: [ {:?} ]", token);
+            if let Some(candidate) = self.discover(i, token.0, &mut variable_idx) {
+                candidates.push(candidate);
+            }
+        }
+
+        for candidate in candidates.iter() {
+            println!("----- FOUND -----");
+            println!("[ {:?} {:?} ]", tokens.get(candidate.variable), tokens.get(candidate.secret));
+            println!("-----  END  -----");
+            println!("");
         }
 
         Ok(())
+    }
+
+    fn discover(&mut self, idx: usize, token: &str, variable_idx: &mut Option<usize>) -> Option<Candidate> {
+        if let Some(var_idx) = variable_idx {
+            if let Some(schema) = &self.scanner.variables_schema {
+                for regex in schema.reg.iter() {
+                    if regex.is_match(token) {
+                        let var = var_idx.clone();
+                        *variable_idx = None;
+                        return Some(Candidate{
+                            variable: var,
+                            secret:idx,
+                        });
+                    }
+                }
+            }
+        }
+
+
+        if let Some(schema) = &self.scanner.variables_schema {
+            if schema.aho.is_match(token) {
+                *variable_idx = Some(idx);
+            }
+        }
+        None
     }
 }
 
 /// Builds the detector.
 #[derive(Debug)]
 pub struct Builder {
-    assignemnts: Vec<String>,
     splits: Vec<String>,
     secret_regexes: Vec<String>,
-    variables: Vec<String>,
+    variables: (Vec<String>, Vec<String>),
 }
 
 impl Builder {
     pub fn new() -> Self {
         Self {
-            assignemnts: Vec::new(),
             splits: Vec::new(),
             secret_regexes: Vec::new(),
-            variables: Vec::new(),
+            variables: (Vec::new(), Vec::new()),
         }
-    }
-
-    /// Populates assigments with given paterns.
-    /// Assignemnts means value after the pattern is asigned to value before the pattern.
-    ///
-    pub fn with_assignemts(&mut self, patterns: &[&str]) -> &mut Self {
-        for pattern in patterns.iter() {
-            self.assignemnts.push(pattern.to_string())
-        }
-        self
     }
 
     /// Populates splits with given paterns.
@@ -111,9 +151,12 @@ impl Builder {
     /// Populates variables with given patterns.
     /// Variables will be precompiled to much a token as a variable.
     ///
-    pub fn with_variables(&mut self, patterns: &[&str]) -> &mut Self {
+    pub fn with_variables(&mut self, patterns: &[&str], regexes: &[&str]) -> &mut Self {
         for pattern in patterns.iter() {
-            self.variables.push(pattern.to_string());
+            self.variables.0.push(pattern.to_string());
+        }
+        for reg in regexes.iter() {
+            self.variables.1.push(reg.to_string());
         }
         self
     }
@@ -123,18 +166,8 @@ impl Builder {
     pub fn try_build_scanner(&self) -> Result<impl Scanner, String>  {
         let mut pre_tokenizers_wrappers = Vec::new();
 
-        for reg in self.secret_regexes.iter() {
-            let res = Split::new(reg.as_str(), SplitDelimiterBehavior::Contiguous, false);
-            if let Err(err) = res {
-                return Err(err.to_string());
-            }
-            if let Ok(eq) = res {
-                pre_tokenizers_wrappers.push(eq.into());
-            }
-        }
-
-        for assignment in self.assignemnts.iter() {
-            let res = Split::new(assignment.as_str(), SplitDelimiterBehavior::Contiguous, false);
+        for var in self.variables.0.iter() {
+            let res = Split::new(var.as_str(), SplitDelimiterBehavior::Contiguous, false);
             if let Err(err) = res {
                 return Err(err.to_string());
             }
@@ -155,15 +188,52 @@ impl Builder {
 
         pre_tokenizers_wrappers.push(CharDelimiterSplit::new(' ').into());
         pre_tokenizers_wrappers.push(CharDelimiterSplit::new('\n').into());
-        pre_tokenizers_wrappers.push(CharDelimiterSplit::new(':').into());
         pre_tokenizers_wrappers.push(CharDelimiterSplit::new(',').into());
 
         let pre_tokenizer = Sequence::new(pre_tokenizers_wrappers);
 
+        let mut variables_schema = None;
+        if self.variables.0.len() > 0 && self.variables.1.len() > 0 {
+            let res = AhoCorasick::new(&self.variables.0);
+            if let Err(err) = res {
+                return Err(err.to_string());
+            }
+            if let Ok(aho) = res {
+                let mut reg = Vec::new();
+                'regex_loop: for rgx in self.variables.1.iter() {
+                    let res = RegexBuilder::new(rgx).build();
+                    if let Ok(r) = res {
+                        reg.push(r);
+                        continue 'regex_loop;
+                    }
+                    if let Err(e) = res {
+                        return Err(e.to_string());
+                    }
+                }
+                variables_schema = Some(VariableSchema {
+                   aho,
+                   reg,
+                });
+            }
+
+        }
+
+        let mut secret_regex = Vec::new();
+
+        for pattern in self.secret_regexes.iter() {
+            let res = RegexBuilder::new(pattern).build();
+            if let Ok(regex) = res {
+                secret_regex.push(regex);
+                continue;
+            }
+            if let Err(e) = res {
+                return Err(e.to_string());
+            }
+        }
+
         Ok(Scan {
-            secret_regex: self.secret_regexes.to_owned(),
-            variables: self.variables.to_owned(),
-            assigments: self.assignemnts.to_owned(),
+            secret_regex,
+            variables_schema,
             pre_tokenizer,
         })
    }
@@ -253,14 +323,10 @@ some passowrd -> alsdkfjaksdj3293u4189389u
 
     #[test]
     fn it_should_create_scanner() {
-        let Ok(mut scanner) = Builder::new().
-            with_assignemts(&[": ", " => ", " = ", " -> "]).
+        let Ok(scanner) = Builder::new().
             with_splits(&[", ", " - ", " + ", " / ", " * "]).
-            with_secret_regexes(&[r#"-----BEGIN PRIVATE KEY-----[\a-zA-Z0-9]*-----END PRIVATE KEY-----"#]).
-            with_variables(&["project_id", "private_key_id", "private_key",
-                    "client_email", "client_id", "auth_uri", "token_uri",
-                    "auth_provider_x509_cert_url","client_x509_cert_url",
-                ]).try_build_scanner() else {
+            with_secret_regexes(&[r#"KEY-----[\a-zA-Z0-9]*-----END"#]).
+            with_variables(&["auth_uri", "token_uri","auth_provider_x509_cert_url"], &[r#"https://[a-zA-Z-0-9./]*"#]).try_build_scanner() else {
                     assert!(false);
                     return;
                 };
